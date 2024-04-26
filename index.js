1510d769669796fbc44115466b1a550491262456
Gyanendra <gyanen56@gmail.com>
2024-04-26 17:18:20 +1000

    Project completed

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const lists = document.querySelectorAll('.number')
 console.log(lists)
+
+
+
+function update(list){
+    let initialValue = 0
+    let max =  parseInt(list.dataset.number);
+   console.log(max)
+   const i = setInterval(()=>{
+   initialValue++
+   if(max>10000){
+    initialValue+=10;
+   }
+    if(initialValue>max){
+        clearInterval(i);
+        return;
+    }
+    list.textContent=`${initialValue}+`
+  
+   },1)
+  
+   
+}
+
+  
+
+
 lists.forEach((list)=>{
-    console.log(list.dataset.number)
+   update(list)
 })
